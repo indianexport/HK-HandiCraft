de3bdcdb7f554bab9feadc836586dbedceb0eff2
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-08-03 11:45:59 +0530

    added more pages

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,15 @@
+import AboutSection from "../components/AboutSection";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const AboutPage = () => {
+    return (
+        <>
+            <Navbar />
+            <AboutSection />
+            <Footer />
+        </>
+    );
+}
+
+export default AboutPage;
---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,15 @@
+import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const ContactPage = () => {
+    return (
+        <>
+            <Navbar />
+            <ContactSection />
+            <Footer />
+        </>
+    );
+}
+
+export default ContactPage;
